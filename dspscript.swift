761dd0fff41dcdd24a104185d6208b3212c8dc8c
Salman <Salman@Asifs-MacBook-Pro-2.local>
2020-01-28 22:20:18 -0500

    reading and writing to a file in swift

--- dspscript.swift
+++ dspscript.swift
@@ -1,11 +1,35 @@
-//
-//  helpers.swift
-//  
-//
-//  Created by Salman on 1/27/20.
-//
+#!/usr/bin/swift
 
 import Foundation
 
 
-print("hello")
+func readFile(_ path: String) -> Int {
+
+    let text = "some text"
+
+    if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+        
+        print("doing some sick file stuff")
+
+        let fileURL = dir.appendingPathComponent(path)
+
+        print(fileURL)
+        //writing
+        do {
+            try text.write(to: fileURL, atomically: false, encoding: .utf8)
+        }
+        catch{ print("AAAAH")}
+
+        //reading
+        do {
+            let text2 = try String(contentsOf: fileURL, encoding: .utf8)
+            print(text2)
+        }
+        catch {print("big problem boys")}
+
+
+    }
+    return 0
+}
+
+readFile("Capstone/sample.txt")
