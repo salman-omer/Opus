6ec296a2b6c3fdda4e56ff9447bf5aac8c9fffdb
Salman <Salman@Asifs-MacBook-Pro-2.local>
2020-01-29 22:31:53 -0500

    initial fft testing

--- dspscript.swift
+++ dspscript.swift
@@ -1,35 +1,35 @@
 #!/usr/bin/swift
 
 import Foundation
+import Accelerate
 
+// following this tutorial https://developer.apple.com/documentation/accelerate/finding_the_component_frequencies_in_a_composite_sine_wave
 
-func readFile(_ path: String) -> Int {
+// The following creates the array, called signal, that contains 10 component sine waves:
+let n = vDSP_Length(2048)
 
-    let text = "some text"
+let frequencies: [Float] = [1, 5, 25, 30, 75, 100, 300, 500, 512, 1023]
 
-    if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-        
-        print("doing some sick file stuff")
+let tau: Float = .pi * 2
 
-        let fileURL = dir.appendingPathComponent(path)
-
-        print(fileURL)
-        //writing
-        do {
-            try text.write(to: fileURL, atomically: false, encoding: .utf8)
-        }
-        catch{ print("AAAAH")}
-
-        //reading
-        do {
-            let text2 = try String(contentsOf: fileURL, encoding: .utf8)
-            print(text2)
-        }
-        catch {print("big problem boys")}
-
-
+let signal: [Float] = (0 ... n).map { index in 
+    frequencies.reduce(0) { accumulator, frequency in
+        let normalizedIndex = Float(index) / Float(n)
+        return accumulator + sin(normalizedIndex * frequency * tau)
     }
-    return 0
 }
 
-readFile("Capstone/sample.txt")
+// signal is an array that represents composite sine waves constructed from the frequencies array.
+// Essentially we are going from a list of frequencies to a sinusoid
+
+
+// Now we need to create initial values for fft setup. According to the docs, this is really
+// expensive so we only want to do it once per program run, for example once the app opens
+
+let log2n = vDSP_Length(log2(Float(n)))
+
+guard let fftSetUp = vDSP.FFT(log2n: log2n, radix: .radix2, ofType: DSPSplitComplex.self) 
+    else { 
+        fatalError("Can't create FFT Setup") 
+    }
+
