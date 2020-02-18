974e95faa6a1cc64c3a71970b2edda861662a1f7
Salman <s.k.omer@hotmail.com>
2020-02-17 21:47:42 -0500

    Remove all of the old sample UI and code from the ViewController. Then, add a new button to trigger recording for later analysis

--- MicrophoneAnalysis/ViewController.swift
+++ MicrophoneAnalysis/ViewController.swift
@@ -10,188 +10,127 @@ import AudioKit
 import AudioKitUI
 import UIKit
 import Dispatch
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    @IBOutlet private var frequencyLabel: UILabel!
-    @IBOutlet private var amplitudeLabel: UILabel!
-    @IBOutlet private var noteNameWithSharpsLabel: UILabel!
-    @IBOutlet private var noteNameWithFlatsLabel: UILabel!
-    @IBOutlet private var audioInputPlot: EZAudioPlot!
+    @IBOutlet weak var recordButton: UIButton!
 
-    var mic: AKMicrophone!
-    var tracker: AKFrequencyTracker!
-    var silence: AKBooster!
+
 
     let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
     let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
     let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
     
     let samplingFrequency = 16000
-
-    func setupPlot() {
-        let plot = AKNodeOutputPlot(mic, frame: audioInputPlot.bounds)
-        plot.translatesAutoresizingMaskIntoConstraints = false
-        plot.plotType = .rolling
-        plot.shouldFill = true
-        plot.shouldMirror = true
-        plot.color = UIColor.blue
-        audioInputPlot.addSubview(plot)
-
-        // Pin the AKNodeOutputPlot to the audioInputPlot
-        var constraints = [plot.leadingAnchor.constraint(equalTo: audioInputPlot.leadingAnchor)]
-        constraints.append(plot.trailingAnchor.constraint(equalTo: audioInputPlot.trailingAnchor))
-        constraints.append(plot.topAnchor.constraint(equalTo: audioInputPlot.topAnchor))
-        constraints.append(plot.bottomAnchor.constraint(equalTo: audioInputPlot.bottomAnchor))
-        constraints.forEach { $0.isActive = true }
-    }
+    
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        AKSettings.audioInputEnabled = true
-        AKSettings.sampleRate = AudioKit.engine.inputNode.inputFormat(forBus: 0).sampleRate
         
-        mic = AKMicrophone()
-        tracker = AKFrequencyTracker(mic)
-        silence = AKBooster(tracker, gain: 0)
+        recordingSession = AVAudioSession.sharedInstance()
+
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        print("Recording allowed")
+                    } else {
+                        print("Recording blocked")
+                    }
+                }
+            }
+        } catch {
+            print("Failed to record")
+        }
     }
+    
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    
 
-        AudioKit.output = silence
+    @IBAction func pressRecordButton(_ sender: Any) {
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
         do {
-            try AudioKit.start()
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+
+            recordButton.setTitle("Tap to Stop", for: .normal)
         } catch {
-            AKLog("AudioKit did not start!")
+            finishRecording(success: false)
         }
-        setupPlot()
-        Timer.scheduledTimer(timeInterval: 0.1,
-                             target: self,
-                             selector: #selector(ViewController.updateUI),
-                             userInfo: nil,
-                             repeats: true)
-        Timer.scheduledTimer(timeInterval: 1/samplingFrequency,
-                             target: self,
-                             selector: #selector(processInputMethod2),
-                             userInfo: nil,
-                             repeats: true)
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            // recording failed :(
+        }
     }
     
     
-    var seconds: Int = 0;
-    var initialTime: Double = 0
-    var timeList: [Double] = []
-    var list: [Double] = [];
-    var start = DispatchTime.now()
-    var end = DispatchTime.now()
-    
-    @objc func processInputMethod1() {
-        
-        list.append(tracker.amplitude)
-        if list.count == samplingFrequency {
-            end = DispatchTime.now()
-            
-            let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
-            let timeInterval = Double(nanoTime) / 1_000_000_000
-            
-            if seconds == 0 {
-                initialTime = timeInterval
-            } else {
-                timeList.append(timeInterval)
-            }
-            
-            seconds = seconds + 1
-            if timeList.count == 60 {
-                let average = timeList.reduce(0, {res, val in res + val}) / timeList.count
-                print("Average processing time over \(timeList.count) seconds is \(average) (not including initial processing time")
-                print("Initial processing time is \(initialTime)")
-            
-            }
-            //print(list.count)
-            list = []
-            start = DispatchTime.now()
-            
-        }
-        
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
-    var iterations = 0
     
-    @objc func processInputMethod2() {
-        iterations = iterations + 1
-        if iterations == samplingFrequency {
-            end = DispatchTime.now()
-            
-            let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
-            let timeInterval = Double(nanoTime) / 1_000_000_000
-            
-            if seconds == 0 {
-                initialTime = timeInterval
-            } else {
-                timeList.append(timeInterval)
-            }
-            
-            seconds = seconds + 1
-            if timeList.count == 60 {
-                let average = timeList.reduce(0, {res, val in res + val}) / timeList.count
-                print("Average processing time over \(timeList.count) seconds is \(average) (not including initial processing time")
-                print("Initial processing time is \(initialTime)")
-            
-            }
-            
-            iterations = 0
-            
-            start = DispatchTime.now()
-            
-        }
-        timeList.append(tracker.amplitude)
-        if timeList.count > samplingFrequency {
-            timeList.removeFirst()
-        }
-        
-    }
 
     @objc func updateUI() {
-        if tracker.amplitude > 0.1 {
-            frequencyLabel.text = String(format: "%0.1f", tracker.frequency)
-
-            var frequency = Float(tracker.frequency)
-            while frequency > Float(noteFrequencies[noteFrequencies.count - 1]) {
-                frequency /= 2.0
-            }
-            while frequency < Float(noteFrequencies[0]) {
-                frequency *= 2.0
-            }
-
-            var minDistance: Float = 10_000.0
-            var index = 0
-
-            for i in 0..<noteFrequencies.count {
-                let distance = fabsf(Float(noteFrequencies[i]) - frequency)
-                if distance < minDistance {
-                    index = i
-                    minDistance = distance
-                }
-            }
-            let octave = Int(log2f(Float(tracker.frequency) / frequency))
-            noteNameWithSharpsLabel.text = "\(noteNamesWithSharps[index])\(octave)"
-            noteNameWithFlatsLabel.text = "\(noteNamesWithFlats[index])\(octave)"
-        }
-        amplitudeLabel.text = String(format: "%0.2f", tracker.amplitude)
-    }
-
-    // MARK: - Actions
-
-    @IBAction func didTapInputDevicesButton(_ sender: UIBarButtonItem) {
-        let inputDevices = InputDeviceTableViewController()
-        inputDevices.settingsDelegate = self
-        let navigationController = UINavigationController(rootViewController: inputDevices)
-        navigationController.preferredContentSize = CGSize(width: 300, height: 300)
-        navigationController.modalPresentationStyle = .popover
-        navigationController.popoverPresentationController!.delegate = self
-        self.present(navigationController, animated: true, completion: nil)
+//        if tracker.amplitude > 0.1 {
+//            frequencyLabel.text = String(format: "%0.1f", tracker.frequency)
+//
+//            var frequency = Float(tracker.frequency)
+//            while frequency > Float(noteFrequencies[noteFrequencies.count - 1]) {
+//                frequency /= 2.0
+//            }
+//            while frequency < Float(noteFrequencies[0]) {
+//                frequency *= 2.0
+//            }
+//
+//            var minDistance: Float = 10_000.0
+//            var index = 0
+//
+//            for i in 0..<noteFrequencies.count {
+//                let distance = fabsf(Float(noteFrequencies[i]) - frequency)
+//                if distance < minDistance {
+//                    index = i
+//                    minDistance = distance
+//                }
+//            }
+//            let octave = Int(log2f(Float(tracker.frequency) / frequency))
+//            noteNameWithSharpsLabel.text = "\(noteNamesWithSharps[index])\(octave)"
+//            noteNameWithFlatsLabel.text = "\(noteNamesWithFlats[index])\(octave)"
+//        }
+//        amplitudeLabel.text = String(format: "%0.2f", tracker.amplitude)
     }
 
 }
@@ -210,18 +149,15 @@ extension ViewController: UIPopoverPresentationControllerDelegate {
     }
 }
 
-// MARK: - InputDeviceDelegate
 
-extension ViewController: InputDeviceDelegate {
+// MARK: - AVAudioRecorderDelegate
 
-    func didSelectInputDevice(_ device: AKDevice) {
-        do {
-            try mic.setDevice(device)
-        } catch {
-            AKLog("Error setting input device")
+
+extension ViewController: AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
         }
     }
-
 }
-
-
